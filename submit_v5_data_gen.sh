a069dd99c4f428ffec1130db7c6108d0f519636a
ayush-kumar-prog <ayushkumargithub@gmail.com>
2025-11-30 21:10:40 +0000

    Set 7-day time limit for data generation job

--- submit_v5_data_gen.sh
+++ submit_v5_data_gen.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=32G
-#SBATCH --time=12:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --output=v5_data_gen_%j.out
 #SBATCH --error=v5_data_gen_%j.err
 
